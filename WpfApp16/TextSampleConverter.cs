002adfb8fc394c8dee9c723296cf66554b318ba8
Alex Pushkin <ifc@suz-ppk.ru>
2023-01-18 09:36:08 +1000

    Добавьте файлы проекта.

---
+++ WpfApp16/TextSampleConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp16
+{
+    public class MtoKMConverter:
+        IValueConverter
+    {
+        // из кода в интерфейс
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double.TryParse(value.ToString(), out double m);
+            return m / 1000.0d + " км";
+        }
+
+        // из интерфейса в код
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class MtoSMConverter :
+        IValueConverter
+    {
+        // из кода в интерфейс
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double.TryParse(value.ToString(), out double m);
+            return m * 100 + " см";
+        }
+
+        // из интерфейса в код
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
